384e61fb46a73a33b48eaaec467b86360bfa0d1d
hbh7 <hbh7@hbh7.com>
2020-08-03 23:31:18 -0400

    Better arg parsing, fix on checkout

--- createTestInstance.sh
+++ createTestInstance.sh
@@ -4,12 +4,26 @@
 # Argument Validation #
 #######################
 
+# Name the variables
 BRANCH=$1
 COMMIT=$2
 
+# Make sure exactly 2 arguments are passed
 if [ $# -ne 2 ]; then
     echo "Invalid Arguments Specified, Aborting."; exit 1
 fi
+
+# Only allowing A-Z, a-z, 0-9, -, _, / in branch names
+if [[ "${BRANCH}" =~ [^-_/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789] ]]; then
+    echo "Invalid Branch, Aborting."; exit 1
+fi
+
+# Only allowing a-z, 0-9 in commit IDs
+if [[ "${COMMIT}" =~ [^abcdefghijklmnopqrstuvwxyz0123456789] ]]; then
+    echo "Invalid Commit, Aborting."; exit 1
+fi
+
+echo "Arguments Validated."
 
 ###############
 # Basic Setup #
@@ -18,13 +32,6 @@ fi
 # Echo out what we're doing
 echo "Creating instance for $BRANCH at $COMMIT..."
 
-# Some input validation to be safe
-if [[ "${BRANCH}" =~ [^-_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/] ]]; then
-    echo "Invalid Branch, Aborting."; exit 1
-fi
-if [[ "${COMMIT}" =~ [^-_ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789/] ]]; then
-    echo "Invalid Commit, Aborting."; exit 1
-fi
 
 # Enter the folder to spin up an instance
 cd ~/CICD_TestInstances || { echo "Test Instances Folder Missing, Aborting."; exit 1; }
@@ -42,7 +49,7 @@ git clone https://github.com/PollBuddy/PollBuddy || { echo "Repo Cloning Failed,
 cd PollBuddy || { echo "Branch.Commit Folder Missing, Aborting."; exit 1; }
 
 # Checkout the commit
-git checkout "COMMIT" || { echo "Commit Checkout Failed, Aborting."; exit 1; }
+git checkout "$COMMIT" || { echo "Commit Checkout Failed, Aborting."; exit 1; }
 
 ############################
 # Configure Instance Setup #
