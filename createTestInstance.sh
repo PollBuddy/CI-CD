0cbdb9532c4d4e0e94605fb15b8325ac6c9c96b9
hbh7 <hbh7@hbh7.com>
2020-08-04 00:28:05 -0400

    Delete mention of branch, no longer using

--- createTestInstance.sh
+++ createTestInstance.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
+########################################################################################
+# This script is designed to be called with a commit ID. It will then take that,       #
+# clone the repo, check out the commit, and configure and start the app based on that. #
+########################################################################################
+
 echo "Starting createTestInstance.sh Script..."
 
 #######################
 # Argument Validation #
 #######################
 
-# Name the variables
-BRANCH=$1
+# Name the variable
 COMMIT=$2
 
-# Make sure exactly 2 arguments are passed
-if [ $# -ne 2 ]; then
+# Make sure exactly 1 argument is passed
+if [ $# -ne 1 ]; then
     echo "Invalid Arguments Specified, Aborting."; exit 1
-fi
-
-# Only allowing A-Z, a-z, 0-9, -, _, / in branch names
-if [[ "${BRANCH}" =~ [^-_/ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789] ]]; then
-    echo "Invalid Branch, Aborting."; exit 1
 fi
 
 # Only allowing a-z, 0-9 in commit IDs
@@ -25,34 +24,34 @@ if [[ "${COMMIT}" =~ [^abcdefghijklmnopqrstuvwxyz0123456789] ]]; then
     echo "Invalid Commit, Aborting."; exit 1
 fi
 
-echo "Arguments Validated."
+echo "Argument Validated."
 
 ###############
 # Basic Setup #
 ###############
 
 # Echo out what we're doing
-echo "Creating instance for branch '$BRANCH' at commit '$COMMIT'"
+echo "Creating instance for commit '$COMMIT'"
 
 # Enter the folder to spin up an instance
 cd ~/CICD_TestInstances || { echo "Test Instances Folder Missing, Aborting."; exit 1; }
 
 # If for some reason this job was being rerun, we'll want to delete the old folder. We don't care if it fails of course
-rm -rf "$BRANCH.$COMMIT"
+rm -rf "$COMMIT"
 
 # Create a folder for this instance
 echo "Creating folder for this instance"
-mkdir "$BRANCH.$COMMIT"
+mkdir "$COMMIT"
 
 # Enter it
-cd "$BRANCH.$COMMIT" || { echo "Branch.Commit Folder Missing, Aborting."; exit 1; }
+cd "$COMMIT" || { echo "Commit Folder Missing, Aborting."; exit 1; }
 
 # Clone the repo
 echo "Cloning repo"
 git clone https://github.com/PollBuddy/PollBuddy || { echo "Repo Cloning Failed, Aborting."; exit 1; }
 
 # Enter it
-cd PollBuddy || { echo "Branch.Commit Folder Missing, Aborting."; exit 1; }
+cd PollBuddy || { echo "Repo Folder Missing, Aborting."; exit 1; }
 
 # Checkout the commit
 echo "Checking out commit"
