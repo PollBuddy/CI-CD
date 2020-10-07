55b87d340ae20a8a0b9b014f54aa5a10cc4a219e
hbh7 <hbh7@hbh7.com>
2020-10-07 00:07:13 -0400

    Major renovations for handling PRs

--- deployTestInstance.sh
+++ deployTestInstance.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-########################################################################################
-# This script is designed to be called with a commit ID. It will then take that,       #
-# clone the repo, check out the commit, and configure and start the app based on that. #
-########################################################################################
+#################################################################################################
+# This script is designed to be called with a commit ID and a mode string (either ISSUE or PR). #
+# It will then take that, clone the repo, check out the commit or PR, and configure and start   #
+# the app based on that.                                                                        #
+#################################################################################################
 
 # Requires packages: docker, docker-compose, procmail, git, sed, tac, potentially others depending on your distro
 
@@ -14,55 +15,76 @@ echo "Starting deployTestInstance.sh Script..."
 #####################
 
 function finish {
-  # Remove lock file
+  # Remove lock files
   rm -f ~/deployTestInstance.lock
+  rm -f ~/deployTestInstance-"$ID".lock
 }
 trap finish EXIT
 
 # Try to acquire a lock every 5 seconds, not continuing until then.
 # Given that this normally is run by GitHub, this should end up terminated by them if it never gets a lock
 echo "Acquiring lock..."
+
+# Acquire unique lock so that we can have parallel builds that don't interfere with each other
 lockfile -5 ~/deployTestInstance.lock
+
 
 
 #######################
 # Argument Validation #
 #######################
 
-# Name the variable
-COMMIT=$1
+# Name the variables
+ID=$1
+MODE=$2
 
-# Make sure exactly 1 argument is passed
-if [ $# -ne 1 ]; then
-    echo "Invalid Arguments Specified, Aborting."; exit 1
+# Make sure exactly 2 arguments are passed
+if [ $# -ne 2 ]; then
+    echo "Invalid Number Of Arguments Specified, Aborting."; exit 1
 fi
 
-# Only allowing a-z, 0-9 in commit IDs
-if [[ "${COMMIT}" =~ [^abcdefghijklmnopqrstuvwxyz0123456789] ]]; then
-    echo "Invalid Commit, Aborting."; exit 1
+# Validate MODE and ID (depending on MODE)
+if [ "$MODE" = "ISSUE" ]; then
+
+   # Only allow a-z, 0-9 in commit IDs
+  if [[ "${ID}" =~ [^abcdefghijklmnopqrstuvwxyz0123456789] ]]; then
+      echo "Invalid Commit ID, Aborting."; exit 1
+  fi
+
+elif [ "$MODE" = "PR" ]; then
+
+  # Only allow 0-9 in PR Numbers
+  if [[ "${ID}" =~ [^0123456789] ]]; then
+      echo "Invalid PR Number, Aborting."; exit 1
+  fi
+
+else
+  echo "Invalid Mode Specified, Aborting."; exit 1
 fi
 
-echo "Argument Validated."
+echo "Arguments Validated."
+
+
 
 ###############
 # Basic Setup #
 ###############
 
 # Echo out what we're doing
-echo "Creating instance for commit '$COMMIT'"
+echo "Creating instance for commit/PR '$ID'"
 
 # Enter the folder to spin up an instance
 cd ~/CICD_TestInstances || { echo "Test Instances Folder Missing, Aborting."; exit 1; }
 
 # If for some reason this job was being rerun, we'll want to delete the old folder. We don't care if it fails of course
-rm -rf "$COMMIT"
+rm -rf "$ID"
 
 # Create a folder for this instance
 echo "Creating folder for this instance"
-mkdir "$COMMIT"
+mkdir "$ID"
 
 # Enter it
-cd "$COMMIT" || { echo "Commit Folder Missing, Aborting."; exit 1; }
+cd "$ID" || { echo "Commit Folder Missing, Aborting."; exit 1; }
 
 # Clone the repo
 echo "Cloning repo"
@@ -71,9 +93,33 @@ git clone https://github.com/PollBuddy/PollBuddy || { echo "Repo Cloning Failed,
 # Enter it
 cd PollBuddy || { echo "Repo Folder Missing, Aborting."; exit 1; }
 
-# Checkout the commit
-echo "Checking out commit"
-git checkout "$COMMIT" || { echo "Commit Checkout Failed, Aborting."; exit 1; }
+# Checkout depending on mode
+if [ "$MODE" = "ISSUE" ]; then
+
+  # Checkout the commit
+  echo "Checking out commit"
+  git checkout "$ID" || { echo "Commit Checkout Failed, Aborting."; exit 1; }
+
+elif [ "$MODE" = "PR" ]; then
+
+  # Checkout the PR
+  echo "Checking out PR"
+
+  # Configure remotes to allow checking out PR merge commits
+  # See: https://gist.github.com/piscisaureus/3342247
+
+  # Add remote URL
+  sed -i '/fetch = +refs\/heads\/\*:refs\/remotes\/origin\/\*/a \\tfetch = +refs\/pull\/*\/head:refs\/remotes\/origin\/pr\/*' .git/config || { echo "PR Checkout Failed at SED, Aborting."; exit 1; }
+
+  # Pull PR
+  git fetch origin "refs/pull/$ID/head:pr/$ID" || { echo "PR Checkout Failed at FETCH, Aborting."; exit 1; }
+
+  # Checkout PR
+  git checkout "pr/$ID" || { echo "PR Checkout Failed and CHECKOUT, Aborting."; exit 1; }
+
+fi
+
+
 
 ############################
 # Configure Instance Setup #
@@ -89,7 +135,7 @@ cp PollBuddy-Server/frontend/.env.example PollBuddy-Server/frontend/.env || { ec
 
 # Modify frontend's .env file
 # Update REACT_APP_BACKEND_URL
-sed -i "/REACT_APP_BACKEND_URL/c\REACT_APP_BACKEND_URL=https://dev-$COMMIT.pollbuddy.app/api" PollBuddy-Server/frontend/.env  || { echo "Frontend SED Failed, Aborting."; exit 1; }
+sed -i "/REACT_APP_BACKEND_URL/c\REACT_APP_BACKEND_URL=https://dev-$ID.pollbuddy.app/api" PollBuddy-Server/frontend/.env  || { echo "Frontend SED Failed, Aborting."; exit 1; }
 
 # Done configuring frontend environment variables
 echo "Frontend environment variables configured"
@@ -121,7 +167,7 @@ fi
 echo "Acquired port $PORT"
 
 # Store our commit ID in the port file for use in cleanup tasks
-echo "$COMMIT" > "$HOME/dev-site-ports/$PORT"
+echo "$ID" > "$HOME/dev-site-ports/$PORT"
 
 # Write into docker compose file
 sed -i "s/7655:80/$PORT:80/g" docker-compose.yml  || { echo "Docker SED Failed, Aborting."; exit 1; }
@@ -155,13 +201,13 @@ echo "Configuring environment variables complete"
 echo "Starting instance"
 
 # Build containers
-docker-compose -p $COMMIT build --parallel || { echo "Docker-Compose Build Failed, Aborting."; exit 1; }
+docker-compose -p $ID build --parallel || { echo "Docker-Compose Build Failed, Aborting."; exit 1; }
 
 # Just in case this is a rerun, try to shut down previous containers
-docker-compose -p $COMMIT down
+docker-compose -p $ID down
 
 # Start it
-docker-compose -p $COMMIT up -d || { echo "Docker-Compose Up Failed, Aborting."; exit 1; }
+docker-compose -p $ID up -d || { echo "Docker-Compose Up Failed, Aborting."; exit 1; }
 
 # We're done!
 echo "Instance is now running"
@@ -174,11 +220,11 @@ echo "Instance is now running"
 echo "Configuring dev site"
 
 # Copy example configuration file
-cp ~/PollBuddy.app/webserver/conf.d/TEMPLATE.conf.ignore "$HOME/dev-site-configs/$COMMIT.conf" || { echo "Template NGINX Config Copy Failed, Aborting."; exit 1; }
+cp ~/PollBuddy.app/webserver/conf.d/TEMPLATE.conf.ignore "$HOME/dev-site-configs/$ID.conf" || { echo "Template NGINX Config Copy Failed, Aborting."; exit 1; }
 
 # Edit configuration file
-sed -i "s/TEMPLATE_COMMITID/$COMMIT/g" "$HOME/dev-site-configs/$COMMIT.conf"  || { echo "NGINX SED Failed, Aborting."; exit 1; }
-sed -i "s/TEMPLATE_PORT/$PORT/g" "$HOME/dev-site-configs/$COMMIT.conf"  || { echo "NGINX SED Failed, Aborting."; exit 1; }
+sed -i "s/TEMPLATE_IDID/$ID/g" "$HOME/dev-site-configs/$ID.conf"  || { echo "NGINX SED Failed, Aborting."; exit 1; }
+sed -i "s/TEMPLATE_PORT/$PORT/g" "$HOME/dev-site-configs/$ID.conf"  || { echo "NGINX SED Failed, Aborting."; exit 1; }
 
 # We're done!
 echo "Dev site configured"
@@ -205,7 +251,7 @@ echo "Dev site restarted"
 
 # We're done!
 echo "Deployment completed successfully!"
-echo "Deploy Link: https://dev-$COMMIT.pollbuddy.app/"
+echo "Deploy Link: https://dev-$ID.pollbuddy.app/"
 
 # Exit
 exit 0
