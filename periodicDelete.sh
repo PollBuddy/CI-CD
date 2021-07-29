a2a42851effc216b232ba6929dd87e12ad6cbc16
hbh7 <hbh7@hbh7.com>
2021-07-29 15:08:59 -0400

    V1 of periodicDelete.sh

--- periodicDelete.sh
+++ periodicDelete.sh
@@ -2,6 +2,66 @@
 
 ########################################################################################
 # This script is designed to be called with no arguments as a periodic cron job. It    #
-# will check for instances that have been spun down, and delete them after a set time  #
-# period elapses. They would have to be manually recreated to be spun up again.        #
+# will check for instances that have been shut down, and delete them after a set time  #
+# period elapses (currently, 4 weeks). They would have to be manually/automatically    #
+# recreated to be spun up again.                                                       #
 ########################################################################################
+
+###############
+# Basic Setup #
+###############
+
+# Echo out what we're doing
+echo "Checking for any instances to delete (current time: $(date))."
+
+# Enter the folder of instances
+cd ~/CICD_TestInstances || { echo "Test Instances Folder Missing, Aborting."; exit 1; }
+
+# Save current time
+NOW=$(date +"%s")
+
+# Calculate time offset. seconds * minutes * days * 4 weeks
+(( TIMEOUT = 60*60*24*28 ))
+
+# Loop through each folder and check if it's time to delete it
+for entry in "."/*
+do
+  entry=${entry:2}
+  echo "$entry"
+
+  # Check the modified time
+  INSTANCETIME=$(stat -c %Y "$entry")  # Checks the modified time of the instance folder
+
+  # Compare
+  (( DAYSOLD = ( ( NOW - INSTANCETIME ) / ( 60*60*24 ) ) ))
+  echo "Instance is $DAYSOLD days old ($(stat -c %y "$entry"))."
+
+  # Check if too old
+  if (( ( NOW - INSTANCETIME ) > TIMEOUT )); then
+      echo "Shutting down and deleting instance."
+
+      # Enter the folder
+      cd "$entry/PollBuddy" || { echo "Failed to cd into instance, aborting."; exit 1; }
+      docker-compose -p "$entry" down -v
+      docker-compose -p "$entry" rm -s -f -v
+
+      cd ../../ || { echo "Failed to cd out of instance, aborting."; exit 1; }
+      echo "Instance has been shut down and containers removed."
+
+      # Delete the folder
+      rm -rf "$entry"
+      echo "Instance folder has been deleted."
+
+  else
+      echo "Instance does not need to be deleted."
+  fi
+
+  echo "---"
+
+done
+
+echo "Instance check complete on $(date)."
+echo ""
+
+# Exit
+exit 0
